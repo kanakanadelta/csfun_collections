c6beb5637a68af8b63c64ad96b2c5c95ac431a15
Rafael Tongson <rtongson@peppymiint.local>
2019-10-14 13:27:31 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ARRAYS //
+            int[] ints = new int[10];
+            for(int i = 0; i < 10; i++) 
+            {
+                ints[i] = i;
+            }
+
+            string[] strings = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+
+            bool[] bools = new bool[10];
+            for(int j = 0; j < 10; j++)
+            {
+                if(j % 2 == 0)
+                {
+                    bools[j] = true;
+                }
+                else
+                {
+                    bools[j] = false;
+                }
+            }
+
+            // LISTS //
+            List<string> flavors = new List<string>();
+            flavors.Add("Chocolate");
+            flavors.Add("Vanilla");
+            flavors.Add("Strawberry");
+            flavors.Add("Cookies n' Cream");
+            flavors.Add("Pistachio");
+            flavors.Add("Rocky Road");
+            Console.WriteLine($"Length of flavors is {flavors.Count}");
+            Console.WriteLine($"The third flavor is {flavors[3]}");
+
+            // DICTIONARIES //
+            
+        }
+    }
+}
